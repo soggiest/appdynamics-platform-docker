13a668cabb9d5c7317ea80d45b380d8b4ccfab02
Mark Prichard <mqprichard@gmail.com>
2015-08-13 10:54:17 -0700

    Added notice to install, start and stop scripts

--- stop-appdynamics.sh
+++ stop-appdynamics.sh
@@ -1,4 +1,13 @@
+# This script is provided for illustration purposes only.
+#
+# To build these Docker containers, you will need to download the following components:
+# 1. An appropriate version of the Oracle Java 7 JDK
+#    (http://www.oracle.com/technetwork/java/javase/downloads/index.html)
+# 2. Correct versions for the AppDynamics Controller and EUM Server (64-bit Linux)
+#    (https://download.appdynamics.com)
+
 #!/bin/bash
+
 echo "Stopping AppDynamics Controller"
 echo "*******************************"
 echo
